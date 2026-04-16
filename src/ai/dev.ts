89247d6ea7764211470cb8e9208eac468dc97085
Shanuj Kumar <shanujkumar775@gmail.com>
2026-04-16 18:30:17 +0000

    now make feature works properly

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -3,3 +3,5 @@ config();
 
 import '@/ai/flows/ai-security-chatbot.ts';
 import '@/ai/flows/fake-message-detector.ts';
+import '@/ai/flows/link-scanner-flow.ts';
+import '@/ai/flows/deepfake-verifier-flow.ts';
